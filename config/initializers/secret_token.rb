f6cbd69b4403654ff25a472dc26efd2c67937710
deciob <deciob@deciob.(none)>
2013-03-09 11:01:12 +0000

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartopops::Application.config.secret_token = '28381061c1a117d4c1a6862f3c86219ab19b05b81ce4eb2ab2a4e5958e5f18f3bf1cc8adbd0d40e25596ae7c844ed898edc50fc6f52a13e23d1e7c37b88891d5'
